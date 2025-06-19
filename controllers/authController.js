50ed7e1bc9eb7e0e943f3d8253adfc575630f95c
kainat <Kainatn002@gmail.com>
2025-06-19 21:14:56 +0200

    added routes, and controllers

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.js";
+import bcrypt from "bcrypt";
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ success: false, error: "User Not Found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res
+        .status(404)
+        .json({ success: false, error: "Password Not Matched" });
+    }
+
+    const token = jwt.sign(
+      { _id: user._id, role: user.role },
+      process.env.JWT_KEY,
+      { expiresIn: "10d" }
+    );
+
+    res.status(200).json({
+      success: true,
+      token,
+      user: { _id: user._id, name: user.name, role: user.role },
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+export { login };
---
+++ routes/auth.js
@@ -0,0 +1,8 @@
+import express, { Router } from "express";
+import { login } from "../controllers/authController.js";
+
+const router = express.Router();
+
+router.post("/login", login);
+
+export default router;
